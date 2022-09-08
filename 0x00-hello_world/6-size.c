3056799cdfa89633ef6d412be8fcd965b3a16aaf
Barackhalogen <barackhalogen@gmail.com>
2022-09-08 11:43:29 -0700

    6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
 
+
+
 /**
-* main - print the stringin the put function
-* 
-* Description: using the main function
-* this program prints "the size of various types on the computer it is compiled and run on."
-* Return: 0
-*/
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
 int main(void)
+
 {
-	char c;
-	int i;
-	long a;
-	long long b;
+
+	char a;
+
+	int b;
+
+	long int c;
+
+	long long int d;
+
 	float f;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(a));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(b));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+
+
 	return (0);
+
 }
