f6f26d72eec961a4191132453b96d2b273f2604b
Barackhalogen <barackhalogen@gmail.com>
2022-09-08 10:26:10 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,15 @@
+#!/bin/bash
 #include <stdio.h>
 
-
-
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
