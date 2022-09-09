d3a9868f196a440abfe55fcca0a462f706dd0cf9
Barackhalogen <barackhalogen@gmail.com>
2022-09-08 20:09:12 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
 
 /**
 * main - entry point
-* Description: prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,"
-* followed by a new line, to the standard error
+* Description: prints "and that piece of art is useful,
+* Dora Korpar, 2015-10-19"
 * Return: Always 1
 */
 int main(void)
