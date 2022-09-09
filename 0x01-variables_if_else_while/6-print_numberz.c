f9ed54ebcd8511dbdff8ad307cdfdbef6839c771
Barackhalogen <barackhalogen@gmail.com>
2022-09-09 16:13:42 -0700

    Task 6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
-* main - Another code goes here
-* Description: using the main function
-* this program  prints all single digit numbers of base 10 starting from 0
-* Return 0;
+* main - about to script another dope code
+* Description - using the main function
+* this program prints all single digit numbers of base 10 starting from 0
+* Return: 0
 */
 int main(void)
 {
-char ch;
-for (ch = '0'; ch <= '9'; ch++)
-putchar(ch);
-putchar ('\n');
+int d;
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
 return (0);
 }
