723be8d044f855752d0a32451c80d819f9038ac7
Barackhalogen <barackhalogen@gmail.com>
2022-09-09 17:00:07 -0700

    Task 9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Start the code here 
+* Description: using the main function
+* this program  prints all possible combinations of single-digit numbers
+* Return: 0
+*/
+int main(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+if (c != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -6,3 +6,4 @@ a program that prints the alphabet in lowercase
  a program that prints all single digit numbers of base 10 starting from 0
 7. a program that prints the lowercase alphabet in reverse
 8. a program that prints all the numbers of base 16 in lowercase
+9.  a program that prints all possible combinations of single-digit numbers.
