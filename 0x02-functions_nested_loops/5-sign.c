4e52ce0a04ca5a2f63dcddd61b9aa23959f7a61d
Barackhalogen <barackhalogen@gmail.com>
2022-09-13 09:26:59 -0700

    Task 5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * print_sign - function Checks for -ve, +ve or 0 integers
-* @n: This is a number
+* @n: This is a number assigned
 * Return: 0 if zero, 1 if > 0 and -1 if < 0
 */
 int print_sign(int n)
@@ -16,6 +16,6 @@ else if (n < 0)
 _putchar('-');
 return (-1);
 }
-_putchar('0')
+_putchar('0');
 return (0);
 }
