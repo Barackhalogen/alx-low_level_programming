1d521c37d3e3b6b42883bb4c26eebd1d7a6a3a02
Barackhalogen <barackhalogen@gmail.com>
2022-09-13 09:21:17 -0700

    Task 5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
-* main - I dey code my guy
-* Description: using the main function and print_sign function
-* this program Checks for -ve, +ve or 0 integers
-* Result: 0 if zero, 1 if > 0 and -1 if < 0
+* print_sign - function Checks for -ve, +ve or 0 integers
+* @n: This is a number
+* Return: 0 if zero, 1 if > 0 and -1 if < 0
 */
 int print_sign(int n)
 {
@@ -17,6 +16,6 @@ else if (n < 0)
 _putchar('-');
 return (-1);
 }
-_putchar('0');
+_putchar('0')
 return (0);
 }
