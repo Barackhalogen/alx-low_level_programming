c4b3e72f1b1bf6bdd1c036aa991c85aef06e8ffb
Barackhalogen <barackhalogen@gmail.com>
2022-09-13 22:06:44 -0700

    Task 0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - This file must test that the function
+* positive_or_negative(): prints positive or negative number and
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
