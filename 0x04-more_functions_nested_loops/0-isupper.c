9e82ebf1316594ab314155f69aa744227a8ea4ed
Barackhalogen <barackhalogen@gmail.com>
2022-09-15 04:06:00 -0700

    Task 0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* _isupper - this function checks for uppercase ASCII characters
+* @c: This is an ASCII character
+* This code dey sweet me die
+* Return: 0 if uppercase, else 1
+*/
+int _isupper(int c)
+{
+if (isupper(c))
+return (1);
+return (0);
+}
