586efa377ab91091be156283574ba1beeed090cf
Barackhalogen <barackhalogen@gmail.com>
2022-09-19 11:57:48 -0700

    Task 10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -25,16 +25,11 @@ while (str[i] >= '0' && str[i] <= '9')
 if (base > INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7))
 {
 if (sign == 1)
-{
 return (INT_MAX);
-}
 else
-{
 return (INT_MIN);
-}
 }
 base = 10 * base + (str[i++] - '0');
 }
-
 return (base * sign);
 }
