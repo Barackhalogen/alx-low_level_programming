95747d6ddb1f72fe14dc0a97ea065522bf1e6e02
Barackhalogen <barackhalogen@gmail.com>
2022-09-19 10:53:11 -0700

    Task 6. Half the lies they tell about me aint true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* puts2 - print alternating chars of string
+* @str: string, is the parameter pased
+* Return: Void
+*/
+void puts2(char *str)
+{
+int i;
+
+i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
