53ae2b22a7dd66fbf851668596a4856b5bc3c43e
Barackhalogen <barackhalogen@gmail.com>
2022-10-12 06:33:33 -0700

    Task 3. A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+* op_add - Add two numbers
+* @a: The first number
+* @b: The second number
+* Return: The sum of a and b
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* op_sub - Finds the difference of two numbers
+* @a: The first number, a parameter
+* @b: The second number, a param
+* Return: The diff of a and b
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+* op_mul - Multiplies two numbers
+* @a: The first number, a param
+* @b: The second number, a param
+* Return: The product of a and b
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+* op_div - Divides two numbers
+* @a: The first number, a param
+* @b: The second number, a param
+* Return: The quotient of a and b
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+* op_mod - Finds the modulus of two numbers
+* @a: The first number, a param
+* @b: The second number, a param
+* Return: The modulus of a and b
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
