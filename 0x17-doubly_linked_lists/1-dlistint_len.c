4283700b61a2cc1e54f76f689ef47a7ae28aacd5
Barackhalogen <barackhalogen@gmail.com>
2022-12-09 19:32:07 -0800

    Task 1. List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,19 @@
-#!/usr/bin/python3
+#include "lists.h"
 
-def size_t dlistint_len(const dlistint_t *h)
-	temp = self.head
-	i = 0
-	while (temp != None):
-		i += 1
-		temp = temp.next
-		return i
+/**
+ * dlistint_len - Counts the number of elements in a linked dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ * Return: The number of elements in the dlistint_t list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
